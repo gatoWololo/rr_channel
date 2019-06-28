3288df7d3e161ce5bbef1aa83d566b05a2be94d4
gatowololo <gatowololo@gmail.com>
2019-06-28 10:09:34 -0700

    Added support for testing via examples.
    Run `python3 run_examples_as_tests.py` to run all examples 10 times,
    ensuring the output is idential between record and replay executions.

---
+++ run_examples_as_tests.py
@@ -0,0 +1,62 @@
+import os
+import subprocess
+
+import sys
+
+def cargo_run_record(example):
+    env = os.environ.copy()
+    env["RR_CHANNEL"] = "record"
+    return subprocess.check_output("cargo run --example " + example,
+                                   env=env, shell=True, stderr=subprocess.DEVNULL)
+
+def cargo_run_replay(example):
+    env = os.environ.copy()
+    env["RR_CHANNEL"] = "replay"
+    return subprocess.check_output("cargo run --example " + example,
+                                   env=env, shell=True, stderr=subprocess.DEVNULL)
+
+# We're calling a partial cargo command, we expect it to fail and return the
+# avaliable exampes.
+try:
+    _ = subprocess.check_output(["cargo", "run", "--example"],
+                                     stderr=subprocess.STDOUT)
+    # Should jump to "except" case here!
+    print('Running "cargo run --examples" was expected to fail...')
+    sys.exit(1)
+except subprocess.CalledProcessError as e:
+    output = e.output
+    cargo_run_failed = True
+
+# print("Output: ", output)
+
+loop_done = False
+examples = []
+
+# Strip the whitespace from the stderr output and split by lines.
+for line in output.strip().split(b'\n'):
+    # print(line)
+    if line.strip() == b"Available examples:":
+        loop_done = True
+        continue
+    if loop_done:
+        examples.append(line.strip())
+
+if not loop_done:
+    print('Running "cargo run --examples" did not return the expected output format')
+    sys.exit(1)
+
+# print("Examples: ", examples)
+
+for example in examples:
+    example = example.decode("utf-8")
+    print("Running example: {}".format(example))
+
+    # Run multiple times to be sure...
+    for _ in range(10):
+        record = cargo_run_record(example)
+        replay = cargo_run_replay(example)
+
+        if record != replay:
+            print("Outputs between record and replay differ!")
+            sys.exit(1)
+    print("[10/10] OK!")
