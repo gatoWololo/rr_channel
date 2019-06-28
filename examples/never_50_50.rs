ffe7f0e372ace01914f7222a0a6dc2d6e5f338b4
gatowololo <gatowololo@gmail.com>
2019-06-28 16:04:57 -0700

    Added never example/test.

---
+++ examples/never_50_50.rs
@@ -0,0 +1,33 @@
+/// Stolen from crossbeam_channel::never example.
+/// Modified to 50/50 timeout.
+use std::thread;
+use std::time::{Duration, Instant};
+use rand::thread_rng;
+use rand::Rng;
+
+fn main(){
+    let (s, r) = rr_channels::unbounded();
+
+    rr_channels::spawn(move || {
+        thread::sleep(Duration::from_secs(1));
+        s.send(1).unwrap();
+    });
+
+    // Suppose this duration can be a `Some` or a `None`.
+    let duration =
+        if thread_rng().gen_bool(0.50) {
+            Some(Duration::from_millis(100))
+        } else {
+            None
+        };
+
+    // Create a channel that times out after the specified duration.
+    let timeout = duration
+        .map(|d| rr_channels::after(d))
+        .unwrap_or(rr_channels::never());
+
+    rr_channels::select! {
+        recv(r) -> msg => println!("Message: {:?}", msg),
+        recv(timeout) -> _ => println!("timed out"),
+    }
+}
